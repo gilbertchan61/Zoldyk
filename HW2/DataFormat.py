cce9ba592ba7e821125ee7ee182cad41feb4e767
Sh-Abd <65983454+Sh-Abd@users.noreply.github.com>
2021-10-04 17:21:20 -0700

    new try

--- HW2/DataFormat.py
+++ HW2/DataFormat.py
@@ -1,7 +1,13 @@
 # importing panda library
 import pandas as pd
-import json, csv
+import json, csv  
+from xml.dom import minidom 
+import os 
 import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ET
+from lxml import etree
+import sys
+
 
 
 def makeCSV(fileNme):
@@ -32,17 +38,74 @@ def makeJSON(fileNme):
         jsonf.write(json.dumps(dict1, indent= 4))
 
 
-
 def makeXML(fileNme):
-    # users_list = ["GeeksForGeeks", "Arka", "Computer Science", "Engineering", "Portal"]
-    # usrconfig = ET.Element("usrconfig")
-    # usrconfig = ET.SubElement(usrconfig, "usrconfig")
-    # for user in range(len( users_list)):
-    pass
+    root = etree.Element('data')
+    with open("HW2/NFL_stats.txt", encoding="UTF-8") as xmlf:
+        csv_reader = csv.DictReader(xmlf, delimiter='\t')
+        header = next(csv_reader)
+        for row in csv_reader:
+            eg = etree.SubElement(root, 'eg')
+        for h, v in zip(header, row):
+            etree.SubElement(eg, h).text = v
 
-  
+    xml_file = 'text_as_xml.xml'
+    f = open(xml_file, "w")
+    f.write(etree.tostring(root))
+    f.close()
 
+# def makeXML(fileNme):
+#     root = etree.Element('data')
+#     with open(fileNme, encoding="ISO-8859-1") as xmlf:
+#         xml_file = 'text_as_xml.xml'
+#         txtReader = csv.reader(root , delimiter = '\t')
+#         data = []
+#         temp = []
+#         next(txtReader)
+#         with open(xml_file, 'w') as fh:
+#             for i in txtReader:
+#                 fh.write(f"""<Year>{i[0]}</Year>
+#     <Player>{i[1]}</Player>
+#     <Age>{i[2]}</Age>
+#     <Hometown>{i[3]}</Hometown>
+#     <State>{i[4]}</State>
+#     <Tm>{i[5]}</Tm>
+#     <G>{i[6]}</G>
+#     <GS>{i[7]}</GS>
+#     <Cmp>{i[8]}</Cmp>
+#     <Att>{i[9]}</Att>
+#     <Yds>{i[10]}</Yds>
+#     <TD>{i[11]}</TD>
+#     <Int>{i[12]}</Int>
+#     <Att>{i[13]}</Att>
+#     <Yds>{i[14]}</Yds>
+#     <Y/A>{i[15]}</Y/A>
+#     <TD>{i[16]}</TD>
+#     <Rec>{i[17]}</Rec>
+#     <Yds>{i[18]}</yDs>
+#     <Y/R>{i[19]}</Y/R>
+#     <TD>{i[20]}</TD>
+#     <FantPos>{i[21]}</FantPos>
+#     <FantPt>{i[22]}</FantPt>
+#     <Height (inches)>{i[23]}</Height (inches)>
+#     <Weight>{i[24]}</Weight>
+#     <College>{i[25]}</College>
+#     <Conference>{i[26]}</Conference>
+#     <College wins>{i[27]}</College wins>
+#     <College losses>{i[28]}</College losses>
+#     <DOB>{i[29]}</DOB>
+#     <Draft Round>{i[30]}</Draft Round>
+#     <Draft Year>{i[31]}</Draft Year>
+#     <Wonderlic>{i[32]}</Wonderlic> 
+#     <40 Yard>{i[33]}</40 Yard> 
+#     <Bench Press>{i[34]}<Bench Press>
+#     <Vert Leap(in)>{i[35]}</Vert Leap(in)>
+#     <Broad Jump(in)>{i[36]}</Broad Jump(in)>
+#     <Shuttle>{i[37]}</Shuttle>
+#     <3Cone>{i[38]}</3Cone>
+#     """)
+#     fh.close()
 
+   
 def makeFile(fileNme, dataType):
     dataType = dataType.lower()
     if(dataType == "c" ):
@@ -54,6 +117,6 @@ def makeFile(fileNme, dataType):
     else:
         print("error")
 
-fileFormat = input("What type of file? CSV,JSON,XML\nC,J,X")
+fileFormat = input("What type of file? CSV,JSON,XML\nC,J,X: ")
 
 makeFile("HW2/NFL_stats.txt", fileFormat)
